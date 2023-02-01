f25b9049dd235e5025e6d5d069b8ecf1ef0f7bd6
Arturs Aboltins <arthuraboltins@gmail.com>
2023-02-01 01:59:42 +0000

    create questions and countdown funct.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,21 +1,45 @@
 var startBtn = document.querySelector("#start");
+var btn = document.querySelector("button");
 var time = document.querySelector("#time");
+var startScreen = document.querySelector("#start-screen");
+var questionsScreen = document.getElementById("questions").style.display = "block";
+questions.style.display = "none";
 
+//start function
 startBtn.addEventListener("click", function () {
+    startGame();
     countdown();
 });
 
-var timeLeft = 76;
-
-function countdown() {
-  var timeInterval = setInterval(function () {
-    timeLeft--;
-    time.textContent = timeLeft;
-
-   if(timeLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timeInterval);
-      // Calls function to create and append image
-  }
-  }, 1000);
+//start the game function
+function startGame() {
+    startScreen.style.display = "none";
+    questions.style.display = "inherit";
+    // questionsScreen.style.display = "block";
+    firstGame();
 }
+
+//end the game function
+function endGame() {
+
+}
+
+// countdown function with the time left var.
+var timeLeft = 76;
+function countdown() {
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        time.textContent = timeLeft;
+
+        if (timeLeft === 0) {
+            // stops execution of action at set interval
+            clearInterval(timeInterval);
+            // calls the end game function.
+            endGame();
+        }
+    }, 1000);
+}
+
+
+
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,25 +1,36 @@
-var allQuestions = document.querySelector("#questions");
+let questions = document.querySelector("#questions");
+let question = document.querySelector("#question-title");
+let choices = document.querySelector("#choices");
 
-allQuestions = [{
-    question: "This is a question?",
-    answers: [
-        "1",
-        "2",
-        "3"
-    ],
-    question: "This is a question?",
-    answers: [
-        "4",
-        "5",
-        "6"
-    ],
-    question: "This is a question?",
-    answers: [
-        "7",
-        "8",
-        "9"
+let allQuestions = [{
+    givenQuestion: "Which operator is used to assign a value to a variable?",
+    givenChoices: [
+        w = "*",
+        c = "=",
+        w = "-",
+        w = "+"
     ]
+},
+{
+    givenQuestion: "Which event occurs when the user clicks on an HTML element?",
+    givenChoices: [
+        c = "onclick",
+        w = "onmouseover",
+        w = "onchange",
+        w = "onmouseclick" 
+    ]
+},
+{
+    givenQuestion: "How does a FOR loop start?",
+    givenChoices: [
+        w = "for i = 1 to 5",
+        w = "for (i <= 5; i++)",
+        w = "for i = 1 to 5",
+        c = "for (i = 0; i <= 5; i++)"
+    ]
+}
 
-}];
+]; ""
 
-console.log(allQuestions);
+
+
