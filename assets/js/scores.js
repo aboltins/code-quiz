fb89fc88c300354d16c9df05f64d8c6ed92ae77c
Arturs Aboltins <arthuraboltins@gmail.com>
2023-02-11 02:08:05 +0000

    sort stored scores order

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -14,13 +14,18 @@ clearBtn.addEventListener("click", clearLocalStorage);
 // Get the high scores stored in local storage
 var storedHighScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+// sorts the stored high scores in descending order 
+storedHighScores.sort(function(a, b) {
+    return b.score - a.score;
+});
 
 // For Loop that goes through the local storage high scores array
 for (var i = 0; i < storedHighScores.length; i++) {
   // Creates a list item for each high score
   var highScoreItem = document.createElement("li");
 //each list items text content is set to the locally stored user + ": " and the score.
-  highScoreItem.textContent = storedHighScores[i].user + ": " + storedHighScores[i].score;
+// users name is capitalized.
+  highScoreItem.textContent = storedHighScores[i].user.toUpperCase() + ": " + storedHighScores[i].score;
   console.log(storedHighScores[i].user + ": " + storedHighScores[i].score);
   // Append each of the created list item to the highscores ordered list
   highScoresList.appendChild(highScoreItem);
